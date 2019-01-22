226c253dc9ee100043e013ffe6d4df4fb21f1035
KhajaMoulali <41651003+KhajaMoulali@users.noreply.github.com>
2019-01-22 14:49:49 +0530

    form crud operations fro beginners step by step
    crud operations

---
+++ App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+/*import logo from './logo.svg';*/
+import './App.css';
+/*import Home from './components/Home.js';*/
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'REACT crud operations',
+      act:0,
+      index:'',
+      datas:[]
+    }
+  }
+
+  componentDidMount(){
+    this.refs.name.focus();
+  }
+
+  fSubmit = (e) =>{
+    e.preventDefault();
+    console.log('try');
+
+    let datas = this.state.datas;
+    let name = this.refs.name.value;
+    let address=this.refs.address.value;
+
+    if(this.state.act === 0){  //new
+    let data={
+      name,address
+    }
+    datas.push(data);
+   }else{                     //update
+    let index = this.state.index;
+    datas[index].name=name;
+    datas[index].address=address;
+   }
+
+    this.setState({
+      datas:datas,
+      act:0
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+
+  fRemove = (i) => {
+    let datas= this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas:datas
+    });
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fEdit= (i) => {
+    let data=this.state.datas[i];
+    this.refs.name.value = data.name;
+    this.refs.address.value = data.address;
+     
+     this.setState({
+        act:1,
+        index:i
+     });
+
+     this.refs.name.focus();
+     this.refs.address.focus();
+  }
+
+  render() {
+    let datas= this.state.datas;
+    return (
+      <div className="App">
+        <h2>{this.props.title}</h2>
+         <form ref="myForm" className="myForm">
+         <input type="text" ref="name" placeholder="your name" className="formField" />
+         <input type="text" ref="address" placeholder="your address" className="formField" />
+          <button onClick={this.fSubmit} className="myButton">Submit</button>
+          </form>
+          <pre>
+           {datas.map((data,i) => 
+            <li key={i} className="myList">
+              {i+1}.{data.name},{data.address}
+              <button onClick={() => this.fRemove(i)} className="myListButton" >remove</button>
+              <button onClick={() => this.fEdit(i)} className="myListButton" >edit</button>
+            </li>
+            )}
+          </pre>
+      </div>
+           );
+      }
+    }
+
+export default App;
